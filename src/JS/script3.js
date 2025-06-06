f83870be9841d06fec33b76e1de47118f4055824
GabrieldPeder <gahpeder0117@gmail.com>
2025-06-06 11:05:23 -0300

    E la vamos nos

--- src/JS/script3.js
+++ src/JS/script3.js
@@ -1,8 +1,14 @@
-const text = `Oi, meu amor...\n\nDesde o primeiro momento, você trouxe cor aos meus dias.  
+const text = `Oi, meu amor...
+
+Desde o primeiro momento, você trouxe cor aos meus dias.  
 Cada risada sua é música para mim.  
-Cada olhar seu me faz acreditar que encontrei o meu lugar.\n\nEssa surpresa é só um pedacinho do quanto você é importante pra mim.  
+Cada olhar seu me faz acreditar que encontrei o meu lugar.
+
+Essa surpresa é só um pedacinho do quanto você é importante pra mim.  
 Do quanto eu penso em você.  
-Do quanto eu te amo.\n\nFeliz Dia dos Namorados, minha vida.  
+Do quanto eu te amo.
+
+Feliz Dia dos Namorados, minha vida.  
 Obrigado por existir. 💖`;
 
 const typewriter = document.getElementById("typewriter");
@@ -13,7 +19,7 @@ function type() {
   if (i < text.length) {
     typewriter.innerHTML += text.charAt(i);
     i++;
-    setTimeout(type, 50); // velocidade da digitação
+    setTimeout(type, 50);
   } else {
     restartBtn.style.display = "inline-block";
   }
